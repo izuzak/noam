5216ffb45f75e4cfe5f0be0c4bbe672e89c030d6
Ivan Zuzak <izuzak@gmail.com>
2012-11-03 09:55:44 +0100

    fix commonjs module exporting

--- src/noam.js
+++ src/noam.js
@@ -17,22 +17,26 @@
  * limitations under the License.
  */
 
-(function (exports) {
+(function (noam) {
   /*
    * Expose the module to the 'world' when loaded via CommonJS/NodeJS, 
    * AMD and <script> tags.
    */
 
   if (typeof module !== 'undefined' && typeof module.exports !== 'undefined') {
-    module.exports = exports; // CommonJS + nodeJS
+    module.exports = noam; // NodeJS
+  }
+  
+  if (typeof exports !== 'undefined' && typeof exports !== "function") {
+    exports.noam = noam; // CommonJs
   }
 
   if (typeof define === "function" && define.amd) {
-    define('noam', [], function () { return exports; } ); // AMD
+    define('noam', [], function () { return noam; } ); // AMD
   }
 
   if (typeof window !== 'undefined') {
-    window.noam = exports;  // <script>
+    window.noam = noam;  // <script>
   }
 }((function () {
   var noam = {};
@@ -501,7 +505,7 @@
     noam.util.HashTable.prototype.containsKey = function(key) {
       return this.get(key) === undefined;
     };
-
+    
     // Iterators internals. The API is below.
     var Iterator = {
       hasNext: function() {
