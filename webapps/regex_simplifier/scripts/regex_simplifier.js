859409366c322f530148ebe620d5d761c6ba29fc
Ivan Zuzak <izuzak@gmail.com>
2013-01-16 09:24:42 +0100

    replace lcs in regex simplifier webapp with jsdiff

--- webapps/regex_simplifier/scripts/regex_simplifier.js
+++ webapps/regex_simplifier/scripts/regex_simplifier.js
@@ -1,68 +1,3 @@
-// taken from http://rosettacode.org/wiki/Longest_common_subsequence#JavaScript
-function lcs(x, y) {
-  var s, i, j, m, n, lcs = [], row = [], c = [], left, diag, latch;
-  //make sure shorter string is the column string
-  if(m<n){s=x;x=y;y=s;}
-  m = x.length;
-  n = y.length;
-  //build the c-table
-  for(j=0;j<n;row[j++]=0);
-  for(i=0;i<m;i++){
-    c[i] = row = row.slice();
-    for(diag=0,j=0;j<n;j++,diag=latch){
-      latch=row[j];
-      if(x[i] == y[j]){row[j] = diag+1;}
-      else{
-        left = row[j-1]||0;
-        if(left>row[j]){row[j] = left;}
-      }
-    }
-  }
-  i--,j--;
-  //row[j] now contains the length of the lcs
-  //recover the lcs from the table
-  while(i>-1&&j>-1){
-    switch(c[i][j]){
-      default: j--;
-        lcs.unshift(x[i]);
-      case (i&&c[i-1][j]): i--;
-        continue;
-      case (j&&c[i][j-1]): j--;
-    }
-  }
-  return lcs.join('');
-}
-
-function lcs_intervals(str1, str2) {
-  var intervals = [];
-
-  var start = 0;
-  var end = 0;
-
-  var i=0;
-
-  while (i < str2.length) {
-    if (str2[i] !== str1[end]) {
-      end += 1
-    } else {
-      if (start !== end) {
-        intervals.push([start, end]);
-      }
-
-      start = end;
-      i += 1;
-      end += 1;
-      start += 1;
-    }
-  }
-
-  if (end < str1.length) {
-    intervals.push([end, str1.length]);
-  }
-
-  return intervals;
-}
-
 var regexCounter = 1;
 var currentRegex = null;
 var sourceRegex = null;
@@ -96,8 +31,6 @@ function simplifyAll() {
 
     if (isDone === false) {
       setTimeout(loop, 0);
-    } else {
-      ;
     }
   };
 
@@ -107,13 +40,11 @@ function simplifyAll() {
 function colorize(source, result) {
   var sourceText = source.text();
   var resultText = result.text();
-  var lcsText = lcs(sourceText, resultText);
 
-  var deletedIntervals = lcsText.length > sourceText.length ? [] : lcs_intervals(sourceText, lcsText);
-  var addedIntervals = resultText.length > lcsText.length ? [] : lcs_intervals(lcsText, resultText);
+  var diff = JsDiff.diffChars(sourceText, resultText);
 
-  colorDiv(source, deletedIntervals, "deletedRegexPart");
-  colorDiv(result, addedIntervals, "addedRegexPart");
+  colorDiv(source, diff, "removed", "deletedRegexPart");
+  colorDiv(result, diff, "added", "addedRegexPart");
 }
 
 function createRegexLabel(regNum) {
@@ -121,13 +52,13 @@ function createRegexLabel(regNum) {
 }
 
 function createSimplifyStepDiv(isInitial, regex) {
-  var outerDiv = $("<div />", { class: "simplifyStep" });
+  var outerDiv = $("<div />", { "class": "simplifyStep" });
 
-  var stepLabel = $("<label />", { class: "regexStep span1" });
-  var stepDiv = $("<div />", { class: "input-block-level monospaceRegex appliedRule", type : "text"});
+  var stepLabel = $("<label />", { "class": "regexStep span1" });
+  var stepDiv = $("<div />", { "class": "input-block-level monospaceRegex appliedRule", type : "text"});
 
-  var regexLabel = $("<label />", { class: "regexCounter span1" });
-  var regexDiv = $("<div />", { class: "input-block-level monospaceRegex regexString", type : "text"});
+  var regexLabel = $("<label />", { "class": "regexCounter span1" });
+  var regexDiv = $("<div />", { "class": "input-block-level monospaceRegex regexString", type : "text"});
 
   var reglab = createRegexLabel(regexCounter);
   regexCounter += 1;
@@ -158,7 +89,7 @@ function simplifyStep() {
   var result = stepResult[0];
   var appliedPatterns = stepResult[1];
 
-  var isDone = result === currentRegex
+  var isDone = result === currentRegex;
   if (isDone) {
     $("#simplifyRegexStep").attr("disabled", true);
     $("#simplifyRegex").attr("disabled", true);
@@ -179,19 +110,20 @@ function simplifyStep() {
 
 $("#simplifyRegexStep").click(simplifyStep);
 
-function colorDiv(div, intervals, cssClass) {
-  var regex = div.text();
-
-  var start = 0;
+function colorDiv(div, parts, type, cssClass) {
   var out = "";
+  var shouldBeTrueOrUndefined = type;
+  var mustBeUndefined = type === "added" ? "removed" : "added";
 
-  for (var i=0; i<intervals.length; i++) {
-    out += regex.slice(start, intervals[i][0]);
-    out += '<font class="' + cssClass + '">' + regex.slice(intervals[i][0], intervals[i][1]) + '</font>';
-    start = intervals[i][1];
+  for (var i=0; i<parts.length; i++) {
+    if (parts[i][mustBeUndefined] === undefined) {
+      if (parts[i][shouldBeTrueOrUndefined] === undefined){
+        out += parts[i].value;
+      } else {
+        out += '<font class="' + cssClass + '">' + parts[i].value + '</font>';
+      }
+    }
   }
-
-  out += regex.slice(start);
 
   div.html(out);
 }
@@ -203,12 +135,11 @@ function simplify_(regex) {
   while (result === regex) {
     config.numIterations += 1;
     config.appliedPatterns = [];
-    var result = noam.re.string.simplify(regex, config);
-    if (config.appliedPatterns.length == 0 || config.appliedPatterns.length == numOfLastStepPatterns)
+    result = noam.re.string.simplify(regex, config);
+    if (config.appliedPatterns.length === 0 ||
+        config.appliedPatterns.length === numOfLastStepPatterns)
       break;
     numOfLastStepPatterns = config.appliedPatterns.length;
   }
   return [result, config.appliedPatterns];
 }
-
-
